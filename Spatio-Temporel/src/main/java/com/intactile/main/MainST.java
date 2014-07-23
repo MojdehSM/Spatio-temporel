b6d628cbcdad98382edb92f7e05f330100edef77
Mojdeh Soltan <mohamadi.mojde@yahoo.com>
2014-07-23 17:46:08 +0200

    Add new querys in the main classe

--- Spatio-Temporel/src/main/java/com/intactile/main/MainST.java
+++ Spatio-Temporel/src/main/java/com/intactile/main/MainST.java
@@ -10,8 +10,8 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.intactile.persistance.CreateOntology;
 
 /**
- * Hello world!
  * 
+ * @author Mojdeh
  */
 public class MainST {
 	public static void main(String[] args) {
@@ -22,7 +22,7 @@ public class MainST {
 	public static void Interogation() {
 		Model model = CreateOntology.model;
 
-		Query query = QueryFactory.create(req2);
+		Query query = QueryFactory.create(req3);
 		QueryExecution qexec = QueryExecutionFactory.create(query, model);
 		try {
 			ResultSet results = qexec.execSelect();
@@ -37,7 +37,7 @@ public class MainST {
 				"./resources/TOntologie.owl", "./resources/SOntologie.owl" };
 		CreateOntology.CreateMemOntologyFromFiles(files, "RDF/XML");
 
-		String[] files2 = { "./resources/Ship1",  "./resources/Ship2", "./resources/Ports" };
+		String[] files2 = { "./resources/Ship1", "./resources/Ship2", "./resources/Ship3", "./resources/Ports" };
 		CreateOntology.CreateMemOntologyFromFiles(files2, "N3");
 
 	}
@@ -45,6 +45,7 @@ public class MainST {
 	static String prefix = "PREFIX afn: <http://jena.hpl.hp.com/ARQ/function#>"
 			+ "PREFIX fn: <http://www.w3.org/2005/xpath-functions#>"
 			+ "PREFIX geo: <http://www.opengis.net/ont/geosparql#>"
+			+ "PREFIX geoS: <http://www.w3.org/2003/01/geo/wgs84_pos#>" 
 			+ "PREFIX geof: <http://www.opengis.net/def/function/geosparql/>"
 			+ "PREFIX gml: <http://www.opengis.net/ont/gml#>"
 			+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
@@ -62,26 +63,9 @@ public class MainST {
 			+ "PREFIX mySx:<http://www.IntactileDESGN.com/ontologies/2014/Example#> "
 			+ "" + "PREFIX spatial: <http://jena.apache.org/spatial#>";
 
-	static String req = prefix + " SELECT ?port ?ship ?when ?position WHERE  "
-			+ "{" + "?ship rdfs:label \"Alpha\" ."
-			+ "?ship rdf:type myS:Ship ."
-			+ "?ship myST:isTimeSliceOf ?timeslice ." 
-			+ ""
-			+ "?timeslice geo:hasGeometry  ?gposition ."
-			+ "?timeslice myST:atTime  ?tposition ."
-			+ "?tposition myT:timeValue  ?when ."
-			+ "?gposition geo:asWKT ?position ."
-			+ "?gposition myS:pointSpeed ?speed ." 
-			+ ""
-			+ "?port rdfs:label \"Port Bastia\" ."
-			+ "?port rdf:type myS:Port ." 
-			+ "?port geo:hasGeometry ?geoPort ."
-			+ "?geoPort geo:asWKT ?wktPort ." 
-			+ ""
-			+ "?position spatial:withinCircle(42.5 9.4 10 'km')"
-			+ "}";
 	
-	static String req2 = prefix
+	
+	static String req1 = prefix
 			+ " SELECT ?ship ?position ?when ?speed WHERE  "
 			+ "{"
 			+ "?ship rdfs:label \"Alpha\" ."
@@ -95,7 +79,7 @@ public class MainST {
 			+ "?gposition myS:pointSpeed ?speed ."
 			+ "}";
 
-	static String req3 = prefix
+	static String req2 = prefix
 			+ " SELECT ?position ?when WHERE  "
 			+ "  { "
 			+ "?ship rdfs:label \"Alpha\" ."
@@ -112,8 +96,51 @@ public class MainST {
 			+ "FILTER ( ?when < \"2014-06-28T13:30:00\"^^xsd:dateTime)."
 			+ "}";
 	
+	static String req3 = prefix
+			+ " SELECT ?ship ?latPosition ?longPosition ?when WHERE"
+			+ "{"
+			+ "?ship rdfs:label \"Gama\" ."
+			+ "?ship rdf:type myS:Ship ."
+			+ "?ship myST:isTimeSliceOf ?timeslice ."
+			+ "?timeslice geo:hasGeometry  ?gposition ."
+			+ "?timeslice myST:atTime  ?tposition ."
+			+ "?tposition myT:timeValue  ?when ."
+			+ "?gposition geoS:lat ?latPosition ."
+			+ "?gposition geoS:long ?longPosition ."
+			//+ "?gposition spatial:nearby(43.184263 9.158967 1 'mi')"
+			+ "}";
+	
+	//_______________________________________________Non RESULT_____________________________________
 
+	
 	static String req4 = prefix
+			+ " SELECT * WHERE"
+			+ "{"
+			+ "?ship rdf:type myS:Ship."
+			+ "?ship spatial:nearby(43.184263 9.158967 10 'km')."
+			+ "?ship rdfs:label ?label"
+			+ "}";
+	
+	static String req5 = prefix + " SELECT ?port ?ship ?when ?position WHERE  "
+			+ "{" + "?ship rdfs:label \"Alpha\" ."
+			+ "?ship rdf:type myS:Ship ."
+			+ "?ship myST:isTimeSliceOf ?timeslice ." 
+			+ ""
+			+ "?timeslice geo:hasGeometry  ?gposition ."
+			+ "?timeslice myST:atTime  ?tposition ."
+			+ "?tposition myT:timeValue  ?when ."
+			+ "?gposition geo:asWKT ?position ."
+			+ "?gposition myS:pointSpeed ?speed ." 
+			+ ""
+			+ "?port rdfs:label \"Port Bastia\" ."
+			+ "?port rdf:type myS:Port ." 
+			+ "?port geo:hasGeometry ?geoPort ."
+			+ "?geoPort geo:asWKT ?wktPort ." 
+			+ ""
+			+ "?gposition spatial:withinCircle(42.5 9.4 10 'km')"
+			+ "}";
+	
+	static String req6 = prefix
 			+ " SELECT ?port ?ship ?when ?position WHERE  "
 			+ "  { "
 			+ "?ship rdfs:label \"Beta\" ."
